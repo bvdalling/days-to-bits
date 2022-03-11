07a0f73e02862f7a7a5e6aafba292288e0fc8335
Brent Dalling <bdalling@sparkfish.com>
2022-03-11 12:25:36 -0700

    Update the package json

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+export declare const FULL = 0;
+export declare const SHORT = 1;
+export declare const NUMBERS = 2;
+declare const Bit: {
+    packDays(days: Number[]): number;
+    unpackDays(decimal: number, type?: number): any[];
+};
+export default Bit;
